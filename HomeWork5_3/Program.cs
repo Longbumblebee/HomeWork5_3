ba5e7df323af215587570762960da9ffa343e200
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-11 19:19:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace HomeWork5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumbers = Console.ReadLine();
+            var inputsArray = randomNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var byteArray = new byte[inputsArray.Length];
+            for (int i = 0; i < inputsArray.Length; i++)
+            {
+                byteArray[i] = byte.Parse(inputsArray[i]);
+            }
+            File.WriteAllBytes("DZ5.3.bin@", byteArray);
+        }
+    }
+}
